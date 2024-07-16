9db2b39e66d1b48d94d408f4f390cd10e3fd680c
FabianGregoire <58170227+FabianGregoire@users.noreply.github.com>
2024-07-16 15:39:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
-const dbUri = 'mongodb+srv://fabian:@Zyq2bcv03@biskit.kldsccb.mongodb.net/?retryWrites=true&w=majority&appName=biskit';
+const dbUri = 'mongodb+srv://fabian:password@biskit.kldsccb.mongodb.net/?retryWrites=true&w=majority&appName=biskit';
 const mangoose = mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
